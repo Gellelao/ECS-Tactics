2830bc07840ba79f41c38ffe8e0fac08270cde7f
Deacon <gellelao@gmail.com>
2023-03-18 11:39:52 +1300

    Remove template stuff from setup file

--- setup.sh
+++ setup.sh
@@ -15,23 +15,3 @@ tar -xf fnalibs.tar.bz2
 cd ..
 
 git submodule update --init --recursive
-
-echo -e "What is your project name?"
-read name
-
-sed -i -e "s/FNAECSTemplate/${name}/g" FNAECSTemplate/FNAECSTemplate.csproj
-sed -i -e "s/FNAECSTemplate/${name}/g" FNAECSTemplate.sln
-sed -i -e "s/FNAECSTemplate/${name}/g" .vscode/launch.json
-sed -i -e "s/FNAECSTemplate/${name}/g" .vscode/tasks.json
-sed -i -e "s/Game1/${name}/g" FNAECSTemplate/Game1.cs
-sed -i -e "s/FNAECSTemplate/${name}/g" FNAECSTemplate/Game1.cs
-sed -i -e "s/FNAECSTemplate/${name}/g" FNAECSTemplate/Systems/ExampleSystem.cs
-sed -i -e "s/FNAECSTemplate/${name}/g" FNAECSTemplate/Components/ExampleComponent.cs
-sed -i -e "s/FNAECSTemplate/${name}/g" FNAECSTemplate/Renderers/ExampleRenderer.cs
-
-mv "FNAECSTemplate.sln" "${name}.sln"
-mv "FNAECSTemplate/FNAECSTemplate.csproj" "FNAECSTemplate/${name}.csproj"
-mv "FNAECSTemplate/Game1.cs" "FNAECSTemplate/${name}.cs"
-mv "FNAECSTemplate" "${name}"
-
-echo -e "\e[32mDone!\e[m"
